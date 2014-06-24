7a9b148eda4658b49a8493762d58e65c380ed48f
Paulo Leal <paulo.leal@ideais.com.br>
2014-06-24 18:34:31 -0300

    bump

---
+++ .yo/app/scripts/controllers/camera.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:CameraCtrl
+ * @description
+ * # CameraCtrl
+ * Controller of the appApp
+ */
+angular.module('appApp')
+  .controller('CameraCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ .yo/app/scripts/controllers/vibration.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:AcelerometerCtrl
+ * @description
+ * # AcelerometerCtrl
+ * Controller of the appApp
+ */
+angular.module('appApp')
+  .controller('VibrationCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
